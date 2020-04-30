ed12182319fee2e49f8018f07d6f9db28d29946c
Arno <46700201+ZHEQIUSHUI@users.noreply.github.com>
2020-04-30 13:18:02 +0800

    Update YoloV3.h

--- yolov3/YoloV3.h
+++ yolov3/YoloV3.h
@@ -1,3 +1,10 @@
+/*
+ * @Description: In User Settings Edit
+ * @Author: zerollzeng
+ * @Date: 2019-08-23 14:50:04
+ * @LastEditTime: 2020-04-30 13:17:43
+ * @LastEditors: ZHEQIUSHUI
+ */
 #ifndef YOLOV3_HPP
 #define YOLOV3_HPP
 
@@ -20,31 +27,32 @@ struct YoloOutDataSt{
 
 
 class YoloV3 {
-public:    
-    YoloV3(const std::string& prototxt, 
-            const std::string& caffeModel,
-            const std::string& engineFile,
-            const std::vector<std::string>& outputBlobName,
-            const std::vector<std::vector<float>>& calibratorData,
-            int maxBatchSize,
-            int mode,
-			int device,
-            int yoloClassNum,
-            int netSize);
+public:
+	YoloV3(const std::string& prototxt,
+		const std::string& caffeModel,
+		const std::string& engineFile,
+		const std::vector<std::string>& outputBlobName,
+		const std::vector<std::vector<float>>& calibratorData,
+		int maxBatchSize,
+		int mode,
+		int device,
+		int yoloClassNum,
+		int netSize);
 
-    ~YoloV3();
+	~YoloV3();
 
-    void DoInference(YoloInDataSt* input,int batchsize, std::vector<std::vector<Bbox>>& output);
+	void DoInference(YoloInDataSt* input, int batchsize, std::vector<std::vector<Bbox>>& output);
 
 protected:
-    Trt* mNet;
-    
-    int mYoloClassNum;
+	Trt* mNet;
 
-    int mNetWidth = 416;
-    int mNetHeight = 416;
+	int mYoloClassNum;
 
-    std::vector<float> mpDetCpu;
+	int mNetWidth = 608;
+	int mNetHeight = 608;
+	int factor = 63883;
+
+	std::vector<float> mpDetCpu;
 };
 
 #endif // YOLOV3_HPP
